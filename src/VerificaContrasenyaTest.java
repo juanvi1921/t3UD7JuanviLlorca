8dfb0ac51bd226236a4a91a8b867fbf4e4f18c41
Juanvi <juanvi1921.cursos@gmail.com>
2024-05-07 15:23:58 +0200

    Commit con test 10 pasado

--- src/VerificaContrasenyaTest.java
+++ src/VerificaContrasenyaTest.java
@@ -90,12 +90,23 @@ class VerificaContrasenyaTest {
          assertFalse(correcta.isEsValida());
          assertEquals(esperada.getMsg(), correcta.getMsg());
      }
-    @Test
+     @Test
      public void testVerificar9() {
          VerificaContrasenya vc = new VerificaContrasenya("hola12A");
          EsCorrecta correcta = vc.verificacion("hola12A");
          EsCorrecta esperada = new EsCorrecta(false, new StringBuilder("La contrasenya ha de tenir almenys 8 caracters\n" +
                  "La contrasenya ha de contenir almenys un caracter especial"));
+         assertFalse(correcta.isEsValida());
+         assertEquals(esperada.getMsg(), correcta.getMsg());
+     }
+
+     @Test
+    public void testVerificar10() {
+         VerificaContrasenya vc = new VerificaContrasenya("holacaracola12");
+         EsCorrecta correcta = vc.verificacion("holacaracola12");
+         EsCorrecta esperada = new EsCorrecta(false,
+                 new StringBuilder("La contrasenya ha de contenir almenys una lletra majuscula\n" +
+                         "La contrasenya ha de contenir almenys un caracter especial"));
          assertFalse(correcta.isEsValida());
          assertEquals(esperada.getMsg(), correcta.getMsg());
      }

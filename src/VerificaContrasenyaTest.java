3fc89c4ff8fc1c024734a21e21e40c65c4c965b3
dam_lmj <juanvi1921.cursos@gmail.com>
2024-05-07 13:40:08 +0200

    Commit con el segundo test pasado

--- src/VerificaContrasenyaTest.java
+++ src/VerificaContrasenyaTest.java
@@ -15,4 +15,15 @@ class VerificaContrasenyaTest {
         assertFalse(correcta.isEsValida());
         Assertions.assertEquals(esperada.getMsg(),correcta.getMsg());
     }
+    @Test
+    public void testVerificar2() {
+        VerificaContrasenya vc = new VerificaContrasenya("hola");
+        EsCorrecta correcta = vc.verificacion("hola");
+        EsCorrecta esperada = new EsCorrecta(false, "La contrasenya ha de tenir almenys 8 caracters\n" +
+                "La contrasenya ha de contenir almenys 2 numeros\n" +
+                "La contrasenya ha de contenir almenys una lletra majuscula\n" +
+                "La contrasenya ha de contenir almenys un caracter especial" );
+        assertFalse(correcta.isEsValida());
+        assertEquals(esperada.getMsg(), correcta.getMsg());
+    }
 }

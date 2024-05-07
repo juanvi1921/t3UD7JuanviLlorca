672fed8e2fd149d22827b540df7af59a363b7e5c
Juanvi <juanvi1921.cursos@gmail.com>
2024-05-07 15:16:36 +0200

    Commit con test 6 pasado

--- src/VerificaContrasenyaTest.java
+++ src/VerificaContrasenyaTest.java
@@ -45,6 +45,8 @@ class VerificaContrasenyaTest {
         EsCorrecta esperada = new EsCorrecta(false, new StringBuilder("La contrasenya ha de tenir almenys 8 caracters\n" +
                 "La contrasenya ha de contenir almenys una lletra majuscula\n" +
                 "La contrasenya ha de contenir almenys un caracter especial"));
+        assertFalse(correcta.isEsValida());
+        assertEquals(esperada.getMsg(), correcta.getMsg());
     }
 
     @Test
@@ -54,5 +56,18 @@ class VerificaContrasenyaTest {
         EsCorrecta esperada = new EsCorrecta(false, new StringBuilder("La contrasenya ha de tenir almenys 8 caracters\n" +
                 "La contrasenya ha de contenir almenys 2 numeros\n" +
                 "La contrasenya ha de contenir almenys un caracter especial"));
+        assertFalse(correcta.isEsValida());
+        assertEquals(esperada.getMsg(), correcta.getMsg());
+    }
+
+    @Test
+    public void testVerificar6() {
+        VerificaContrasenya vc = new VerificaContrasenya("hola!");
+        EsCorrecta correcta = vc.verificacion("hola!");
+        EsCorrecta esperada = new EsCorrecta(false, new StringBuilder("La contrasenya ha de tenir almenys 8 caracters\n" +
+                "La contrasenya ha de contenir almenys 2 numeros\n" +
+                "La contrasenya ha de contenir almenys una lletra majuscula\n"));
+        assertFalse(correcta.isEsValida());
+        assertEquals(esperada.getMsg(), correcta.getMsg());
     }
 }
